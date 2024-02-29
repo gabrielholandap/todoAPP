c1372293dc24b7dccd29fd3720b30249775aef3e
PeterBarboza <pedrom.barboza01@gmail.com>
2024-02-29 16:49:19 -0300

    feat: Adding AuthGuard middleware (decorator)

---
+++ backend/src/shared/middlewares/auth-middleware.ts
@@ -0,0 +1,90 @@
+import { Request, Response, NextFunction } from 'express'
+
+import { UnauthorizedError } from '../errors'
+import { Database } from '../../modules/user/repositories/prisma/database'
+import { AuthUseCases } from '../../modules/auth/use-cases/auth'
+
+const { database } = new Database()
+
+type AuthGuardDescriptorParams = [
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+]
+
+type descriptorMethod = (...args: AuthGuardDescriptorParams) => Promise<Response>
+
+type OptionalAuthenticatedParams = {
+  /**
+   * Default value `false`
+   */
+  optional: boolean
+}
+
+export function AuthGuard({ optional = false }: OptionalAuthenticatedParams) {
+  return function(
+    target: Object,
+    key: Object | Symbol,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod: descriptorMethod = descriptor.value
+
+    descriptor.value = async function (...[req, res, next]: AuthGuardDescriptorParams) {
+      try {
+        const authHeader = req.headers.Authorization
+  
+        if (!optional && !authHeader || authHeader?.constructor !== String) {
+          throw new UnauthorizedError({ message: "Token not provided" })
+        }
+  
+        if (optional && !authHeader) {
+          return await originalMethod.apply(this, [req, res, next])
+        }
+  
+        const parts = authHeader.split(" ")
+  
+        if (parts.length !== 2) {
+          throw new UnauthorizedError({ message: "Invalid token" })
+        }
+  
+        const [scheme, token] = parts
+  
+        if (!/^Bearer$/i.test(scheme)) {
+          throw new UnauthorizedError({ message: "Invalid token" })
+        }
+  
+        const tokenData = AuthUseCases.verifyJwtAccessToken(token)
+        
+        if (!tokenData || !tokenData?.sub) {
+          throw new UnauthorizedError({ message: "Invalid token" })
+        }
+  
+        const user = await database.user.findFirst({
+          where: {
+            id: tokenData.sub
+          },
+          select: {
+            id: true
+          }
+        })
+  
+        if (!user) {
+          throw new UnauthorizedError({ message: "Invalid token" })
+        }
+  
+        const reqUserData = { 
+          userData: {
+            id: tokenData.sub 
+          } 
+        }
+  
+        Object.assign(req, reqUserData)
+  
+        return await originalMethod.apply(this, [req, res, next])
+      } catch (error) {
+        next(error)
+        return
+      }
+    }
+  }
+}
