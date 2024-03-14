a3e0cfb3336dd74214fdd9a624d6f6e8b9060b0c
Gabriel Holanda <gabriel1996hp@hotmail.com>
2024-03-14 15:18:17 -0300

    criacao telas de login

--- frontend/src/PaginaCriacao.jsx
+++ frontend/src/PaginaCriacao.jsx
@@ -2,18 +2,18 @@ import "./stylePaginaDeLogin.css"
 
 export function PaginaCriacao() {
     return (
-        <div className="container">
-        <header>
-            <h1>TodoApp</h1>
+        <div className="containerPaginaDeLogin">
+        <header className="headerPaginaDeLogin">
+            <img src="./imagens/todo-app-logo.svg" alt="" height="20px"/>
         </header>
-        <main>
+        <main className="mainPaginaDeLogin">
             <h2>Crie sua conta</h2>
             <form>
                 <label htmlFor="email">Email</label>
                 <input type="email" name="email" id="" />
                 <label htmlFor="senha">Senha</label>
                 <input type="password" name="senha" id="" />
-                <div className="botoes">
+                <div className="botoesPaginaDeLogin">
                     <button className="btn1">Acessar</button>
                     <button className="btn2">Acessar conta</button>
                 </div>
--- frontend/src/PaginaDeLogin.jsx
+++ frontend/src/PaginaDeLogin.jsx
@@ -2,18 +2,18 @@ import "./stylePaginaDeLogin.css"
 
 export function PaginaDeLogin() {
     return (
-        <div className="container">
-            <header>
-                <h1>TodoApp</h1>
+        <div className="containerPaginaDeLogin">
+            <header className="headerPaginaDeLogin">
+                <img src="./imagens/todo-app-logo.svg" alt="" height="20px"/>
             </header>
-            <main>
+            <main className="mainPaginaDeLogin">
                 <h2>Acesse sua conta</h2>
                 <form>
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" id="" />
                     <label htmlFor="senha">Senha</label>
                     <input type="password" name="senha" id="" />
-                    <div className="botoes">
+                    <div className="botoesPaginaDeLogin">
                         <button className="btn1">Acessar</button>
                         <button className="btn2">Criar conta</button>
                     </div>
